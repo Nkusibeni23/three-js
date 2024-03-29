49e96682cebc1d925c169acdba6bf57961d7b8ae
Your GitHub Username <Your GitHub Email>
2024-03-29 17:50:28 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className=" max-w-2xl mx-auto bg-white">
+      <div className=" font-bold text-2xl text-center">Hello World!</div>
+    </div>
+  );
+}
