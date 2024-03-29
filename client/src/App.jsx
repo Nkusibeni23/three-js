1716d6588121c8ca52923a2e08dcbc9147c0098f
Your GitHub Username <Your GitHub Email>
2024-03-29 18:39:20 +0200

    not complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-export default function App() {
-  return (
-    <div className=" max-w-2xl mx-auto bg-white">
-      <div className=" font-bold text-2xl text-center">Hello World!</div>
-    </div>
-  );
-}
+import * as THREE from "three";
+
+// scene
+const scene = new THREE.Scene();
+
+//  create our sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshBasicMaterial({
+  color: "#00ff83",
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//  camera
+const camera = new THREE.PerspectiveCamera(45, 800, 600);
+scene.add(camera);
+
+// renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(800, 600);
+renderer.render(scene, camera);
